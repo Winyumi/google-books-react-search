28477d8ea5c66d686810fc8f7869dc33af047e0f
Winyumi <winguy2003@hotmail.com>
2020-03-12 19:15:39 -0400

    add nav for search and saved

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,11 +1,16 @@
 import React from "react";
+import "./style.css";
 
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary mb-3">
       <a className="navbar-brand" href="/">
-        Google Books Lookup
+        Google Books
       </a>
+      <div className="navbar-nav">
+        <a className="nav-link" href="/">Search</a>
+        <a className="nav-link" href="/saved">Saved</a>
+      </div>
     </nav>
   );
 }
