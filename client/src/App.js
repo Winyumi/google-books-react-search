21e408534b9c9fb7c37e7fe265819dbaf29ae0c7
Winyumi <winguy2003@hotmail.com>
2020-03-12 20:58:50 -0400

    add routing to saved books

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Search from "./pages/Search";
-import Books from "./pages/Books";
+import Saved from "./pages/Saved";
 import Nav from "./components/Nav";
 
 function App() {
   return (
     <div>
       <Nav />
-      <Search />
+      <Router>
+        <Route exact path="/" component={Search} />
+        <Route exact path="/saved" component={Saved} />
+      </Router>
     </div>
   );
 }
